85b1ab1081e151cc51bdf454aad73d7106ee4b71
bimmerr2019 <gauss2020@protonmail.com>
2024-11-23 10:02:24 -0700

    fixed LSP and border on ncmp

--- config/nvim_josean/lua/josean/plugins/nvim-cmp.lua
+++ config/nvim_josean/lua/josean/plugins/nvim-cmp.lua
@@ -6,37 +6,95 @@ local lspkind = require("lspkind")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-  completion = {
-    completeopt = "menu,menuone,preview,noselect",
-  },
-  snippet = { -- configure how nvim-cmp interacts with snippet engine
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-]>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  -- sources for autocompletion
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" }, -- snippets
-    { name = "buffer" }, -- text within current buffer
-    { name = "path" }, -- file system paths
-  }),
+	completion = {
+		completeopt = "menu,menuone,preview,noselect",
+	},
 
-  -- configure lspkind for vs-code like pictograms in completion menu
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
+	-- Add window settings for borders
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+
+	-- Add view settings
+	view = {
+		entries = "custom",
+		docs = {
+			auto_open = true,
+		},
+	},
+
+	snippet = { -- configure how nvim-cmp interacts with snippet engine
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-]>"] = cmp.mapping.complete(), -- show completion suggestions
+		["<C-e>"] = cmp.mapping.abort(), -- close completion window
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths
+	}),
+
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			maxwidth = 50,
+			ellipsis_char = "...",
+			show_labelDetails = true,
+			before = function(entry, vim_item)
+				-- Keep icon but remove the kind text
+				if vim_item.kind == "Field" then
+					vim_item.kind = " " -- just the icon
+				end
+
+				-- Show source name
+				vim_item.menu = ({
+					nvim_lsp = "[LSP]",
+					luasnip = "[Snippet]",
+					buffer = "[Buffer]",
+					path = "[Path]",
+				})[entry.source.name]
+				return vim_item
+			end,
+			symbol_map = {
+				Text = "󰉿",
+				Method = "󰆧",
+				Function = "󰊕",
+				Constructor = "",
+				Field = "󰜢",
+				Variable = "󰀫",
+				Class = "󰠱",
+				Interface = "",
+				Module = "",
+				Property = "󰜢",
+				Unit = "󰑭",
+				Value = "󰎠",
+				Enum = "",
+				Keyword = "󰌋",
+				Snippet = "",
+				Color = "󰏘",
+				File = "󰈙",
+				Reference = "󰈇",
+				Folder = "󰉋",
+				EnumMember = "",
+				Constant = "󰏿",
+				Struct = "󰙅",
+				Event = "",
+				Operator = "󰆕",
+				TypeParameter = "",
+			},
+		}),
+	},
 })
-
