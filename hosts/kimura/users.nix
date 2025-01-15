9934d515359303595c9b51b73526986c3f21a699
Jedwick <tech@lonlonmail.com>
2025-01-15 12:25:06 -0700

    Added Host Kimura

---
+++ hosts/kimura/users.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  username,
+  newUsers,
+  ...
+}: let
+  inherit (import ./variables.nix) gitUsername;
+  sharedGroup = "shared";
+  sharedMode = "775";
+  
+  # Common user configuration
+  makeUserConfig = description: {
+    homeMode = "${sharedMode}";
+    isNormalUser = true;
+    description = description;
+    password = "password";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+      "scanner"
+      "lp"
+      "scanner"
+      "lpadmin"
+      sharedGroup
+    ];
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true;
+    packages = with pkgs; [];
+  };
+in {
+  users = {
+    groups.${sharedGroup} = {};
+
+    users = {
+      "${username}" = makeUserConfig "${gitUsername}";
+      "${newUsers.alice}" = makeUserConfig "Alice";
+      "${newUsers.bob}" = makeUserConfig "Bob";
+      "${newUsers.carol}" = makeUserConfig "Carol";
+    };
+  };
+}
+
