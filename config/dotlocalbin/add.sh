e0a2faafe0278adf9ea7f8825e0428d86589d85b
bimmerr2019 <gauss2020@protonmail.com>
2025-01-18 04:34:05 -0700

    chg push scripts for gitignore

--- config/dotlocalbin/add.sh
+++ config/dotlocalbin/add.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+# this file got ALOT simpler with gitignore.
+
 # hide my private info with a public stub
-cd $HOME
-public.sh
+# cd $HOME
+# public.sh
 
 #add, commit and push
 cd $HOME/zaneyos
 git add *
 
 # restore my private info and sync. I think the sync is not necessary, so I will comment it out.
-cd $HOME
-private.sh
+# cd $HOME
+# private.sh
 #phoenix sync user
--- config/dotlocalbin/push.sh
+++ config/dotlocalbin/push.sh
@@ -7,17 +7,21 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# This file got alot simpler with gitignore
+#
 # hide my private info with a public stub
-cd $HOME
-public.sh
+# cd $HOME
+# public.sh
 
 #add, commit and push
 cd $HOME/zaneyos
 git add *
+mv .gitignore_sav .gitignore
 git commit -m "$1"
 git push -u origin main
+mv .gitignore .gitignore_sav
 
 # restore my private info and sync. I think the sync is not necessary, so I will comment it out.
-cd $HOME
-private.sh
+# cd $HOME
+# private.sh
 #phoenix sync user
